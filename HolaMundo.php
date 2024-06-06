55db59d05c5bc1ca35a90f672b55a189aafb10f1
Manuel Morales <108149015+Mannumrls@users.noreply.github.com>
2024-06-06 14:32:37 -0600

    prueba commit

--- HolaMundo.php
+++ HolaMundo.php
@@ -1,8 +1,11 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <?php echo '<p>Hola Mundo</p>'; ?>
- </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba de php</title>
+</head>
+<body>
+<?php echo '<p>Hola Mundo</p>'; ?>   
+</body>
 </html>
